6a699c83b6942d5d270b80cc65a424cace30777a
samnart1 <originalsamnart@gmail.com>
2024-09-14 11:33:30 +0200

    hasEvenLength linkedlist vs arraylist

--- coreProjects/Bank Management System/src/Main.java
+++ coreProjects/Bank Management System/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<String> words = new LinkedList<>();
+        words.add("hello");
+        words.add("goodbye");
+        words.add("this");
+        words.add("that");
+        System.out.println(words);
+
+        int i = 0;
+        while (i < words.size()){
+            String element = words.get(i);
+            if (element.length() % 2 == 0) {
+                words.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.println(words);
     }
 }
