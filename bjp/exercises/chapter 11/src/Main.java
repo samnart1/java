90e993df548cbb9dc521cf00b5ca19aed7ec9d01
samnart1 <originalsamnart@gmail.com>
2024-09-16 23:56:19 +0200

    iterators -linkedlist

--- bjp/exercises/chapter 11/src/Main.java
+++ bjp/exercises/chapter 11/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
-        g
+        LinkedList<String> words = new LinkedList<>();
+        words.add("hello");
+        words.add("goodbye");
+        words.add("this");
+        words.add("that");
+
+//        Iterator<E>itr = list.iterator()
+        
+
+    }
+
+    public static void removeEvenLength(LinkedList<String> list) {
+        int i = 0;
+        while (i < list.size()) {
+            String element = list.get(i);
+            if ((element.length() & 2) == 0) {
+                list.remove();
+            } else {
+                i++;
+            }
+        }
     }
 }
