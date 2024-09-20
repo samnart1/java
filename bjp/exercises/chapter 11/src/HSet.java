59f56ce59043fe1c85380d8f6e62afbdcb2faa8a
samnart1 <originalsamnart@gmail.com>
2024-09-20 19:44:20 +0200

    treeset

--- bjp/exercises/chapter 11/src/HSet.java
+++ bjp/exercises/chapter 11/src/HSet.java
@@ -23,18 +23,20 @@ public class HSet {
             System.out.println(". " + word);
         }
 
-        Set<String> words = new TreeSet<>();
-        Scanner scanner = new Scanner(new File("mobydick.txt"));
+//        Set<String> words = new TreeSet<>();
+//        Scanner scanner = new Scanner(new File("mobydick.txt"));
+//
+//        while (scanner.hasNext()){
+//            String word = scanner.next();
+//            word = word.toLowerCase();
+//            if (word.startsWith("a") && word.length() == 3) {
+//                words.add(word);
+//            }
+//        }
 
-        while (scanner.hasNext()){
-            String word = scanner.next();
-            word = word.toLowerCase();
-            if (word.startsWith("a") && word.length() == 3) {
-                words.add(word);
-            }
-        }
+//        System.out.println("Three-letter 'a' words = " + words);
 
-        System.out.println("Three-letter 'a' words = " + words);
+        lotto();
 
     }
 
@@ -42,4 +44,64 @@ public class HSet {
         Set<Integer> set = new HashSet<>(list);
         return set.size() < list.size();
     }
+
+    // Returns the number of unique elements contained in either set1 or set2. Not a good model to follow
+//    public static int totalElements(Set<String> set1, Set<String> set2) {
+//        int count = set1.size();
+//        for (String element : set2) {
+//            if (!set1.contains(element)) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+
+    // returns the number of elements contained in both set1 and set2
+    public static int totalElements(Set<String> set1, Set<String> set2) {
+        Set<String> union = new HashSet<>(set1);
+        union.addAll(set2);
+        return union.size();
+    }
+
+
+
+
+
+
+    public static void lotto() {
+        Set<Integer> winningNumbers = new TreeSet<>();
+        Random r = new Random();
+        while (winningNumbers.size() < 6) {
+            int number = r.nextInt(40) + 1;
+            winningNumbers.add(number);
+        }
+        System.out.println(winningNumbers);
+
+//        Set<Integer> intersection = new TreeSet<>(ticket);
+//        intersection.retainAll(winningNumbers);
+//        int interSize = intersection.size();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
