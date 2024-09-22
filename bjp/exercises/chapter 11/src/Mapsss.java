7c80fa66464eb0a9a23c73184059aa9be6273b30
samnart1 <originalsamnart@gmail.com>
2024-09-22 10:06:12 +0200

    hashmaps

--- bjp/exercises/chapter 11/src/Mapsss.java
+++ bjp/exercises/chapter 11/src/Mapsss.java
@@ -2,25 +2,62 @@ import java.util.*;
 
 public class Mapsss {
     public static void main(String[] args) {
-        Map<String, Double> salaryMap = new HashMap<>();
-        salaryMap.put("Stuart Reges", 10000.00);
-        salaryMap.put("Marty Stepp", 95500.00);
-        salaryMap.put("Jenny", 86753.09);
 
-        double jenSalary = salaryMap.get("Jenny");
-        System.out.printf("Jenny's salary is $%.2f\n", jenSalary);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type a person's name: ");
-        String name = scanner.nextLine();
-        // search the map for the given name
-        if (salaryMap.containsKey(name)) {
-            double salary = salaryMap.get(name);
-            System.out.printf("%s's salary is $%.2f", name, salary);
-        } else {
-            System.out.println("I don't have a record for " + name);
+        Map<Integer, String> ssnMap = new HashMap<>();
+        ssnMap.put(867530912, "Jenny");
+        ssnMap.put(239876305, "Stuart Reges");
+        ssnMap.put(504386382, "Marty Stepp");
+
+        Set<Integer> ssnSet = ssnMap.keySet();
+        for (int ssn : ssnSet) {
+            System.out.println("SSN: " + ssn);
         }
 
-        System.out.println(salaryMap);
+        Collection<String> names = ssnMap.values();
+        for (String name : names) {
+            System.out.println("Name: " + name);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        Map<String, Double> salaryMap = new HashMap<>();
+//        salaryMap.put("Stuart Reges", 10000.00);
+//        salaryMap.put("Marty Stepp", 95500.00);
+//        salaryMap.put("Jenny", 86753.09);
+//
+//        double jenSalary = salaryMap.get("Jenny");
+//        System.out.printf("Jenny's salary is $%.2f\n", jenSalary);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Type a person's name: ");
+//        String name = scanner.nextLine();
+//        // search the map for the given name
+//        if (salaryMap.containsKey(name)) {
+//            double salary = salaryMap.get(name);
+//            System.out.printf("%s's salary is $%.2f", name, salary);
+//        } else {
+//            System.out.println("I don't have a record for " + name);
+//        }
+//
+//        System.out.println(salaryMap);
     }
 }
