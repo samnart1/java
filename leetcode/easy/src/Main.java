747b8ca57809be1f285441a8abccadf43456b33c
samnart1 <originalsamnart@gmail.com>
2024-09-23 21:55:40 +0200

    two sum

---
+++ leetcode/easy/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Two Sums
+        int[] nums0 = {2, 7, 11, 15};
+        int[] nums1 = {3, 2, 4};
+        int[] nums2 = {3, 3};
+
+        int target0 = 9;
+        int target1 = 6;
+        int target2 = 6;
+
+        var output = new TwoSums();
+//        System.out.println(Arrays.toString(output.twoSum(nums2, target2)));
+    }
+}
