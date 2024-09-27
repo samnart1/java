a9bace51442538c0322aae66c10ff20fc75c1c56
samnart1 <originalsamnart@gmail.com>
2024-09-28 01:20:10 +0200

    spring in action, chapter two

--- spring/demo/src/main/java/com/example/demo/tacos/web/OrderController.java
+++ spring/demo/src/main/java/com/example/demo/tacos/web/OrderController.java
@@ -1,10 +1,12 @@
-package com.example.demo.tacos;
+package com.example.demo.tacos.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.example.demo.tacos.TacoOrder;
 
 import lombok.extern.slf4j.Slf4j;
 
