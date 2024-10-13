d907ef812628128956891c3cf675c396a057b719
samnart1 <originalsamnart@gmail.com>
2024-10-13 06:20:06 +0200

    chapter 2

---
+++ spring/taco-cloud/src/main/java/com/example/taco_cloud/Taco.java
@@ -0,0 +1,14 @@
+package com.example.taco_cloud;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    private String name;
+    
+    private List<Ingredient> ingredients;
+    
+}
