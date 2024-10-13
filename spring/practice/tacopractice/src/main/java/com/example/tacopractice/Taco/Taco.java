cc741c9010f970ea760bd5e16021f5bc0c7e70a7
samnart1 <originalsamnart@gmail.com>
2024-10-13 02:01:21 +0200

    restarting spring in action

--- spring/practice/tacopractice/src/main/java/com/example/tacopractice/Taco/Taco.java
+++
@@ -1,9 +0,0 @@
-package com.example.tacopractice.Taco.Taco;
-import java.util.List;
-
-import com.example.tacopractice.Taco.Ingredient;
-
-public class Taco {
-    private String name;
-    List <Ingredient> ingredients;
-}
